d9664ccaf7982e5a81614b420c77493a4b1b5551
Prashant Kesarwani <47484727+pikukesar@users.noreply.github.com>
2021-11-22 17:15:52 -0500

    Update vm.tf

--- terraformFinalProject GITcopy/modules/vm/vm.tf
+++ terraformFinalProject GITcopy/modules/vm/vm.tf
@@ -1,25 +1,25 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "test" {
+  name     = "${var.prefix}-NetInterface"
+  location = "${var.location}"
+  resource_group_name  = "${var.resource_group}"
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = ""
+    subnet_id                     = azurerm_subnet.test.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = azurerm_public_ip.test.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = ""
-  admin_username      = ""
-  network_interface_ids = []
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "${var.prefix}-vm"
+  location            = "${var.location}"
+  resource_group_name  = "${var.resource_group}"
+  size                = "Standard_B1"
+  admin_username      = "testadmin"
+  network_interface_ids = azurerm_network_interface.test.id
   admin_ssh_key {
-    username   = ""
+    username   = "testadmin"
     public_key = "file("${path.module}/ssh-keys/id_rsa.pub")"
     
   }
